7055084278b25630d958ff7a3853fffaa4894078
Areslane <81231071+areslane67@users.noreply.github.com>
2022-04-07 16:37:32 +0100

    js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("chargé");
-    let btn, warning, email, password;
-    let exprmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-    let exprmdp = /^[a-zA-Z0-9._-]{2,15}$/;
-
-    warning = document.querySelector(".menu p");
-    email = document.getElementsByTagName("input")[1].value;
-    password = document.getElementsByTagName("input")[2].value;
-    btn = document.getElementsByTagName("input")[3];
-
+  
+    let exprmail = "areslane67@gmail.com";
+    let exprmdp ="password";
+  
+    warning = document.querySelector(".warning");
+   let email = document.getElementsByTagName("input")[1].value;
+   let password = document.getElementsByTagName("input")[2].value;
+   let btn = document.getElementsByTagName("input")[3];
     btn.addEventListener("click", e => {
         e.preventDefault();
-        Connexion();
+        send();
     });
-    let Connexion = () => {
-
+  
+    let send = () => {
+  
         email = document.getElementsByTagName("input")[1].value;
         password = document.getElementsByTagName("input")[2].value;
-
+        btn = document.getElementsByTagName("input")[3];
+  
         if (email.match(exprmail) && password.match(exprmdp)) {
             warning.classList.add("success");
-            warning.classList.remove("error");
             warning.innerText = "Connexion réussie";
             localStorage.setItem("user", email);
             sessionStorage.setItem("user", "sessionid");
+            alert("Bienvenue "+ password);
+            console.log("Connexion réussie")
         } else if (password.match(exprmdp)) {
-            warning.classList.add("error");
             warning.innerText = "Mail invalide";
             warning.classList.remove("success");
+            console.log("Mail invalide")
         } else if (email.match(exprmail)) {
-            warning.classList.add("error");
             warning.innerText = "Mot de passe invalide";
             warning.classList.remove("success");
+            console.log("Mot de passe invalide");
         } else {
-            warning.classList.add("error");
             warning.innerText = "Mail ou mot de passe incorrecte";
             warning.classList.remove("success");
+            console.log("Mail ou mot de passe incorrecte");
         }
     };
-});
+  });
